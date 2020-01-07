b2e83fa8772dd8901c9bf50a5b0578bf0e27619e
zeroleo12345 <zeroleo12345@163.com>
2020-01-07 12:41:28 +0800

    merge zeroleo12345

--- src/vapplication.cpp
+++ src/vapplication.cpp
@@ -3,22 +3,18 @@
 VApplication::VApplication(int &argc, char **argv)
     : QApplication(argc, argv)
 {
-}
-
-void VApplication::setWindow(VMainWindow * window)
-{
-    this->window = window;
-    connect(this, &QApplication::applicationStateChanged, this, &VApplication::onApplicationStateChanged);
+    connect(this, &QApplication::applicationStateChanged,
+            this, &VApplication::onApplicationStateChanged);
 }
 
 void VApplication::onApplicationStateChanged(Qt::ApplicationState state)
 {
+    Q_UNUSED(state);
 #if defined(Q_OS_MACOS) || defined(Q_OS_MAC)
-    if(state == Qt::ApplicationActive) {
+    if(state == Qt::ApplicationActive && this->window) {
         this->window->show();
         // Need to call raise() in macOS.
         this->window->raise();
     }
 #endif
 }
-
